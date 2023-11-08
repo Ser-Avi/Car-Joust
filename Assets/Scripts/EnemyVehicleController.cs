ac7ff3b62874151e98b8ab75ca1c2a635c1a6275
Ser-Avi <avi_serebrenik@yahoo.com>
2023-11-08 16:36:21 -0600

    Code clean-up

--- Assets/Scripts/EnemyVehicleController.cs
+++ Assets/Scripts/EnemyVehicleController.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+This is attached to each non-player vehicle and manages their behavior.
+Has global vars for velocityForce, carRb, and a var to call mainManager's instance.
+Has methods for initializing vars, managing movement and out of bounds, and to have position be reset.
+*/
 public class EnemyVehicleController : MonoBehaviour
 {
     //Variables
-    [SerializeField] float speed = 15.0f;
     [SerializeField] float velocityForce;
 
     MainManager mainManager;
@@ -20,12 +24,12 @@ public class EnemyVehicleController : MonoBehaviour
     }
 
     // Update is called once per frame
+    //Manages movement and destruction out of bounds.
     void FixedUpdate()
     {
         if (!mainManager.isGamePaused)
         {
             carRb.AddForce(transform.forward * velocityForce);
-            //transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
 
         if (transform.position.y < -10)
@@ -35,12 +39,15 @@ public class EnemyVehicleController : MonoBehaviour
     }
 
     //The reset function resets the car's movement and places it at input.
+    //Not currently used.
+    /*
     public void Reset(Vector3 pos, Quaternion rot)
     {
         transform.SetPositionAndRotation(pos, rot);
         carRb.velocity = Vector3.zero;
         carRb.angularVelocity = Vector3.zero;
     }
+    */
 
 
 }
