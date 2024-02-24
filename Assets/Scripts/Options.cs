da94a46990f895117a41a788490f7732d0ab389a
Ser-Avi <avi_serebrenik@yahoo.com>
2024-02-24 14:55:17 -0600

    Bug Fixes
    Options settings now reflect actual settings. Wall prop prefab spawns properly.

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Options : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TMP_Dropdown gameModeDropdown;
+    [SerializeField] Slider trafficSlider;
+    [SerializeField] Slider lanceSlider;
+    MainManager mainManager;
+    
+    void OnEnable()
     {
+        mainManager = MainManager.Instance;
         
-    }
+        if (mainManager.isGameTimed)
+        {
+            gameModeDropdown.value = 0;
+        }
+        else
+        {
+            gameModeDropdown.value = 1;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        trafficSlider.value = mainManager.spawnRateSetting;
+        lanceSlider.value = mainManager.forceSetting;
     }
 }
